4c9c8194749c9d083d45f8f5e03231fb5e7f887a
Aung Han <aunghan@Aungs-MacBook-Pro.local>
2023-02-04 13:21:16 -0400

    added get routes for tag

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -56,20 +56,6 @@ router.get('/:id', async (req, res) => {
 
 // create new product
 router.post('/', (req, res) => {
-
-  Product.create({
-    product_name: req.body.product_name,
-    price: req.body.price,
-    stock: req.body.stock,
-    category_id: req.body.tagIds
-  })
-    .then((newProduct) => {
-      res.json(newProduct)
-      console.log(newProduct)
-    })
-    .catch((err => {
-      res.json(err);
-    }))
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -78,7 +64,6 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
